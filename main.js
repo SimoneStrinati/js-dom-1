2eccfed7b54953554e7c52df3f10e94691c00c5b
simonestrinati <simone.strinati@outlook.it>
2025-11-10 21:44:20 +0100

    preparazione variabili

--- main.js
+++ main.js
@@ -5,3 +5,23 @@ function clickButton () {
 }
 
 document.querySelector("#bottone").addEventListener("click", clickButton)
+
+
+//Altri due approcci:
+
+
+// Evento funzione anonima:
+//1. document.querySelector("#bottone").addEventListener("clcik", function () {
+//    console.log("cliccato");
+//} );
+
+// Evento con arrow function:
+//2. document.querySelector("#bottone").addEventListener("click", () => {
+//    console.log("cliccato");
+//});
+
+const lampadina = document.getElementById("lampadina");
+const bottone = document.getElementById("bottone");
+
+let accesa = false;
+
