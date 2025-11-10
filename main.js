60cf1781ac63003f0f9434bdaa15c79eed053e42
simonestrinati <simone.strinati@outlook.it>
2025-11-10 22:12:21 +0100

    esercizio terminato

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // Preparazione funzione
 
-function clickButton () {
-    console.log("Click!");
-}
+//function clickButton () {
+//    console.log("Click!");
+//}
 
-document.querySelector("#bottone").addEventListener("click", clickButton)
+//document.querySelector("#bottone").addEventListener("click", clickButton)
 
 
 //Altri due approcci:
@@ -20,8 +20,19 @@ document.querySelector("#bottone").addEventListener("click", clickButton)
 //    console.log("cliccato");
 //});
 
-const lampadina = document.getElementById("lampadina");
+const lampadina = document.getElementById("lampadina-spenta");
 const bottone = document.getElementById("bottone");
 
 let accesa = false;
+
+bottone.addEventListener("click", function() {
+    accesa = !accesa;
+    if (!accesa) {
+        lampadina.src = "./lampadine/img/white_lamp.png";
+        bottone.textContent = "Accendi!";
+    } else {
+        lampadina.src = "./lampadine/img/yellow_lamp.png";
+        bottone.textContent = "Spegni!";
+        }
+})
 
